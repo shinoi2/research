84dbb5eb9c44a5059e193854472e186178965e63
shinoi2 <shinoi@mail.hfut.edu.cn>
2022-05-23 11:04:29 +0800

    update

--- app/server.py
+++ app/server.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import gevent.monkey
+gevent.monkey.patch_all(thread=False)
 from flask import Flask
 from flask_cors import CORS
 from api import api_bp
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    CORS(app)
-    app.config["JSON_AS_ASCII"] = False
-    app.register_blueprint(api_bp)
-
-    app.run(host="0.0.0.0", port=5000)
+CORS(app)
+app.config["JSON_AS_ASCII"] = False
+app.register_blueprint(api_bp)
